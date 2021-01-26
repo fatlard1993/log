468d39b170cbdd108bd62745ebb829a04542f0c5
justfatlard <fatlard1993@gmail.com>
2021-01-26 15:45:40 -0800

    var < const

--- src/index.js
+++ src/index.js
@@ -72,7 +72,7 @@ class Log {
 			};
 		};
 
-		var logger = new Proxy(createLogger('log'), { get(target, method){ return typeof target[method] !== 'undefined' ? target[method] : createLogger(method); } });
+		const logger = new Proxy(createLogger('log'), { get(target, method){ return typeof target[method] !== 'undefined' ? target[method] : createLogger(method); } });
 
 		logger.opts = opts;
 
